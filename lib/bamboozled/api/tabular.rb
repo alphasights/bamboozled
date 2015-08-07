55de3f06749ff72a9eade31e62aeb4de0fd19cc2
Kyle Doherty <kyledoherty22@gmail.com>
2015-08-07 16:04:43 -0400

    Create #update_row method for Tabular class

--- lib/bamboozled/api/tabular.rb
+++ lib/bamboozled/api/tabular.rb
@@ -1,9 +1,16 @@
 module Bamboozled
   module API
     class Tabular < Base
-      require "pry"
       def add_row(table_name: nil, details: nil, employee_id: nil)
         url = "employees/#{employee_id}/tables/#{table_name}"
+        row_data = generate_xml(details)
+        options = { body: row_data }
+
+        request(:post, url, options)
+      end
+
+      def update_row(table_name: nil, details: nil, employee_id: nil, row_id: nil)
+        url = "employees/#{employee_id}/tables/#{table_name}/#{row_id}"
         row_data = generate_xml(details)
         options = { body: row_data }
 
